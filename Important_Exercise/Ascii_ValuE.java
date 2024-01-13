ba38ebeb5973a247c9d1bd99dd509b75beea36e8
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-01-14 01:18:04 +0600

    Create Ascii_ValuE.java

---
+++ Important_Exercise/Ascii_ValuE.java
@@ -0,0 +1,11 @@
+
+package important_java_exercise;
+
+public class Ascii_Value {
+    public static void main(String[] args){
+        int cha='A';
+        System.out.println("the Ascii value of A is : "+cha);
+         System.out.println("the Ascii value of B is : "+(int)'B');
+        
+    }
+}
